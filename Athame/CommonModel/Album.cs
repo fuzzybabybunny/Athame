025e3678c01848da65643725a89f3f1417d2ddce
Joe <joe@svbnet.co>
2016-10-09 01:39:38 +1300

    Add docs

--- Athame/CommonModel/Album.cs
+++ Athame/CommonModel/Album.cs
@@ -6,10 +6,25 @@ namespace Athame.CommonModel
 {
     public class Album
     {
+        /// <summary>
+        /// Service-specific identifier for the album. Not null.
+        /// </summary>
         public string Id { get; set; }
+        /// <summary>
+        /// The album artist. May be null.
+        /// </summary>
         public string Artist { get; set; }
+        /// <summary>
+        /// The album's title. Not null.
+        /// </summary>
         public string Title { get; set; }
+        /// <summary>
+        /// The album's cover artwork URL. Not null.
+        /// </summary>
         public Uri CoverUri { get; set; }
+        /// <summary>
+        /// The album's tracks. May be null.
+        /// </summary>
         public List<Track> Tracks { get; set; }
 
         public int? GetNumberOfTracksOnDisc(int disc)
